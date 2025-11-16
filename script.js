0f515bc44333c33b715c80a662cd02d98775a660
Alessandro <155587729+Ale-ara@users.noreply.github.com>
2025-11-16 17:31:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const btnSaiba = document.querySelector('.btn-saiba');
+const links = document.querySelectorAll('header nav a, .btn-saiba');
 
-if (btnSaiba) {
-    btnSaiba.addEventListener('click', function(e) {
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
         e.preventDefault(); // evita o redirecionamento instantâneo
+        const href = link.href;
+
+        // adiciona a classe de fade-out no body
         document.body.classList.add('fade-out');
 
+        // depois do tempo do fade, redireciona
         setTimeout(() => {
-            window.location.href = btnSaiba.href;
-        }, 600); // duração do fade-out
+            window.location.href = href;
+        }, 600); // tempo igual ao do CSS
     });
-}
+});
